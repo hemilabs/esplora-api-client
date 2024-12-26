acbedb90defc595e781ff2f9e892c51b76479e99
Gonzalo D'elia <gonzalo@hemi.xyz>
2024-12-26 15:26:45 -0300

    Move knip config out of package.json

---
+++ knip.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('knip').KnipConfig}
+ */
+const config = {
+  ignoreDependencies: [
+    "@vitest/coverage-v8",
+    "eslint-config-prettier",
+    "lint-staged",
+  ],
+};
+
+export default config;
